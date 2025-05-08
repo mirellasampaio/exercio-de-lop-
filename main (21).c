2b639b244fcec129853de0bfdc000de6d1649d82
mirellasampaio <mirellavicentesampaio@gmail.com>
2025-05-08 13:57:31 -0300

    Add files via upload

---
+++ main (21).c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <locale.h>
+
+int main () { // ponto de partida
+    int n; // Declara uma variável inteira
+    
+    printf("Digite o numero:\n"); // mostra a informação e os dados 
+    scanf("%d", &n); //Para armazenar á variavel
+    
+    if("nume % 2 == 0") // ele verifica as informações 
+    {
+        printf("é Par!:\n"); // mostra as informações e dados 
+    }
+    else // senão 
+    {
+        printf("É impar!:\n"); // mostra as informações e dados 
+    }
+    
+    return 0; // encerramento do programa 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
