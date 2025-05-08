a4f6a0586a20040e28a7d45d81c130dba4c8c285
mirellasampaio <mirellavicentesampaio@gmail.com>
2025-05-08 13:56:16 -0300

    Add files via upload

---
+++ main (25).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){ // ponto de partida 
+    int i; // declara a variavel  
+    
+    for(i=1;i<=100;i++){// repetição 
+        printf("%d\n", i);//  mostra a informação e os dados 
+    }
+    return 0; // encerramento do programa 
+}
+
+
+
+
+
+
+
+
